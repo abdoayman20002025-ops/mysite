a04428a69faae1de5a9089afe6c545b209b04c37
abdoayman20002025-ops <abdoayman20002025@gmail.com>
2026-03-03 06:22:33 +0200

    main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    let points = 0;
+    let points = localStorage.getItem("points") 
+        ? parseInt(localStorage.getItem("points")) 
+        : 0;
+
     const pointsElement = document.getElementById("points");
+    pointsElement.textContent = points;
 
-    const cards = document.querySelectorAll(".company-card");
+    const buttons = document.querySelectorAll(".company-card");
 
-    cards.forEach(card => {
+    buttons.forEach(card => {
         card.addEventListener("click", function () {
             points += 50;
             pointsElement.textContent = points;
+            localStorage.setItem("points", points);
         });
     });
 
