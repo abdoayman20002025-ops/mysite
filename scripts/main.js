7f16ec83f23d334527c99bd162862671974726d5
abdoayman20002025-ops <abdoayman20002025@gmail.com>
2026-03-03 06:45:38 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,13 +2,27 @@ let points = localStorage.getItem("points")
     ? parseInt(localStorage.getItem("points")) 
     : 0;
 
+let completedOffers = localStorage.getItem("completedOffers") 
+    ? JSON.parse(localStorage.getItem("completedOffers")) 
+    : [];
+
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("points").innerText = points;
 });
 
-function earn(){
+function earn(company){
+
+    if(completedOffers.includes(company)){
+        alert("أنت أخدت نقاط الشركة دي قبل كده 😉");
+        return;
+    }
+
     points += 50;
+    completedOffers.push(company);
+
     localStorage.setItem("points", points);
+    localStorage.setItem("completedOffers", JSON.stringify(completedOffers));
+
     document.getElementById("points").innerText = points;
 }
 
