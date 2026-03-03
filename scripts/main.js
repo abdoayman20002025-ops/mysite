1529ce086412df90ba486eda5e7dc80540278c86
abdoayman20002025-ops <abdoayman20002025@gmail.com>
2026-03-03 06:43:33 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,18 @@
+let points = localStorage.getItem("points") 
+    ? parseInt(localStorage.getItem("points")) 
+    : 0;
+
 document.addEventListener("DOMContentLoaded", function () {
-
-    let points = localStorage.getItem("points") 
-        ? parseInt(localStorage.getItem("points")) 
-        : 0;
-
-    const pointsElement = document.getElementById("points");
-    pointsElement.textContent = points;
-
-    const buttons = document.querySelectorAll(".company-card");
-
-    buttons.forEach(card => {
-        card.addEventListener("click", function () {
-            points += 50;
-            pointsElement.textContent = points;
-            localStorage.setItem("points", points);
-        });
-    });
-
+    document.getElementById("points").innerText = points;
 });
+
+function earn(){
+    points += 50;
+    localStorage.setItem("points", points);
+    document.getElementById("points").innerText = points;
+}
+
+function show(id){
+    document.querySelectorAll(".section").forEach(s=>s.classList.remove("active"));
+    document.getElementById(id).classList.add("active");
+}
